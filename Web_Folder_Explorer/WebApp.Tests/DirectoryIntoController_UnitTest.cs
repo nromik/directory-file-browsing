0c07314efd0ebad2d5bea304b7d41e06f1e36d38
Roman Nikitin <nromik@gmail.com>
2016-08-11 01:46:41 +0300

    Контролер для DirectoryInfo

---
+++ Web_Folder_Explorer/WebApp.Tests/DirectoryIntoController_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using FolderExplorer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Controllers;
+
+namespace WebApp.Tests
+{
+    [TestClass]
+    public class DirectoryIntoController_UnitTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            var controller = new DirectoryInfoController();
+            
+            Assert.IsNull(controller.Get("jhfkjf"));
+        }
+    }
+}
---
+++ Web_Folder_Explorer/WebApp/Controllers/DirectoryInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FolderExplorer;
+
+namespace WebApp.Controllers
+{
+    public class DirectoryInfoController : ApiController
+    {
+        public HttpResponseMessage Get([FromUri]string path = "")
+        {
+            var result = DirectoryDriver.GetFolderInfo(path);
+
+            return result != null ? Request.CreateResponse(HttpStatusCode.OK, result) : Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+    }
+}
