051ffb9300fc23d1e316980dea6cdbae870d3da5
Roman Nikitin <nromik@gmail.com>
2016-08-11 01:44:53 +0300

    Модель - Создано 2 класса для получения информации о папке

--- Web_Folder_Explorer/FolderExplorer/DirectoryDriver.cs
+++ Web_Folder_Explorer/FolderExplorer/DirectoryDriver.cs
@@ -1,14 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Web.Http;
 
 namespace FolderExplorer
 {
-    public class DirectoryDriver
+    public static class DirectoryDriver
     {
 
+        public static FolderInfo GetFolderInfo(string path)
+        {
+
+            if (string.IsNullOrEmpty(path))
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+
+            var result = new FolderInfo();
+
+            result.CurrentDirectory = path;
+
+            // отображение дисков компютера
+            if (path == "root")
+            {
+
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                result.Directoreis = drives.Select(d => d.Name).ToArray();
+
+                return result;
+            }
+
+            if (Directory.Exists(path))
+            {
+
+                result.ParentDirectory = Regex.IsMatch(path, @"^[A-Za-z]+:\\$") 
+                    ? "root" : Directory.GetParent(path).ToString();
+               
+                try
+                {
+                    result.Directoreis = Directory.GetDirectories(path);
+                    result.Files = Directory.GetFiles(path);
+                }
+                // для случаев отсутствия прав доступа просмотра каталога
+                catch (UnauthorizedAccessException exception)
+                {
+                    result.ErrorMessage = exception.Message;
+                }
+                return result;
+            }
+            return null;
+        }
+
+
+    }
+
+    [DataContract]
+    public class FolderInfo
+    {
+        [DataMember(Name = "directoreis")]
+        public string[] Directoreis { get; set; }
+
+        [DataMember(Name = "files")]
+        public string[] Files { get; set; }
+
+        [DataMember(Name = "errorMessage")]
+        public string ErrorMessage { get; set; }
+
+        [DataMember(Name = "parentDirectory")]
+        public string ParentDirectory { get; set; }
+
+        [DataMember(Name = "currentDirectory")]
+        public string CurrentDirectory { get; set; }
     }
 
 }
